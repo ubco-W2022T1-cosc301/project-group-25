c8441e3b29bba4c569074d766cbac3bf2a9d6df0
Ritvik2021 <Ritvik.2021@gmail.com>
2022-11-08 11:38:56 -0800

    Update project_functions1.py

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import numpy as np
 
+excluding_positions = ['GK','GKMF','MFFW','FWMF','MFDF','DFMF','FWDF','DFFW']
+
 def load():
     df = pd.read_csv("../data/raw/Stats.csv",encoding="ISO-8859-1", delimiter = ";")
+    return df
+
+def remove_excluding_positions(df):
+    df = df.drop(df[df.Min < 300].index)
+    for each in excluding_positions:
+        df = df.drop(df[df.Pos == each].index)
+        
     return df
 
 def remove_players(df):    
@@ -18,19 +27,10 @@ def remove_players(df):
     return df
 
 def forwards(df):
-    forwards = df[df.Pos == "FW"]
-    forwards = forwards[["Goals", "Shots", "G/Sh", "Assists", "PasCmp", "Touches", "DriPast", "CarTotDist", "Crs", "GCA", "TklWon", "Press", "Clr", "Int", "Blocks", "Fls", "AerWon"]]
-    
-    return forwards
+    return df[df.Pos == "FW"][["Goals", "Shots", "G/Sh", "Assists", "PasCmp", "Touches", "DriPast", "CarTotDist", "Crs", "GCA", "TklWon", "Press", "Clr", "Int", "Blocks", "Fls", "AerWon"]]
 
 def midfielders(df):
-    midfielders = df[df.Pos == "MF"]
-    midfielders = midfielders[["Goals", "Shots", "G/Sh", "Assists", "PasCmp", "Touches", "DriPast", "CarTotDist", "Crs", "GCA", "TklWon", "Press", "Clr", "Int", "Blocks", "Fls", "AerWon"]]
-    
-    return midfielders
+    return df[df.Pos == "MF"][["Goals", "Shots", "G/Sh", "Assists", "PasCmp", "Touches", "DriPast", "CarTotDist", "Crs", "GCA", "TklWon", "Press", "Clr", "Int", "Blocks", "Fls", "AerWon"]]
 
 def defenders(df):
-    defenders = df[df.Pos == "DF"]
-    defenders = defenders[["Goals", "Shots", "G/Sh", "Assists", "PasCmp", "Touches", "DriPast", "CarTotDist", "Crs", "GCA", "TklWon", "Press", "Clr", "Int", "Blocks", "Fls", "AerWon"]]
-    
-    return defenders
+    return df[df.Pos == "DF"][["Goals", "Shots", "G/Sh", "Assists", "PasCmp", "Touches", "DriPast", "CarTotDist", "Crs", "GCA", "TklWon", "Press", "Clr", "Int", "Blocks", "Fls", "AerWon"]]
